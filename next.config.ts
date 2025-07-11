fcc1170b4fbe58f97ed73892761d20fa61ca3c19
priyambuilds <aichive404@gmail.com>
2025-07-11 15:45:53 +0530

    Changed the structuring of the project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  experimental: {
+  },
+  // Optional: force static generation fallback to avoid mismatch
+  output: "standalone",
 };
 
 export default nextConfig;
